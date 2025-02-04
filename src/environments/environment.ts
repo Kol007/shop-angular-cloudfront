874b2def1a98efa96fd9907eb17b479a1be2df3c
kol007 <koldev007@gmail.com>
2025-02-04 21:48:17 +0200

    update products apiEndpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: false,
   apiEndpoints: {
-    product: 'https://zmpmbudux7.execute-api.us-east-1.amazonaws.com/prod',
+    product: 'https://https://oczawcd0r4.execute-api.us-east-1.amazonaws.com/prod',
     order: 'http://localhost:3000/orders',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
